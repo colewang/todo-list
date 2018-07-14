f4500f67d48bffcc525d075dc4a2ec163ca08c6a
WANG Yu <464963515@qq.com>
2018-07-14 23:54:21 +0800

    [Initial Page Design][wangyu] Added checkbox for todoItem.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,19 @@
 import React from "react";
+import {Checkbox} from 'antd';
 
 class TodoItem extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    onChange(e) {
+        console.log(`checked = ${e.target.checked}`);
+    }
+
     render() {
-        return this.props.value;
+        return (
+            <Checkbox onChange={this.onChange}>{this.props.value}</Checkbox>
+        )
     }
 }
 
