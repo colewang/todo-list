ec87594cc6ef9b028a83a68eef2d964b33bc4522
WANG Yu <464963515@qq.com>
2018-07-14 23:54:00 +0800

    [Initial Page Design][wangyu] Added tab for different statu of todoItem.

--- src/components/TodoBox.js
+++ src/components/TodoBox.js
@@ -2,16 +2,29 @@ import React from 'react';
 import {Row, Col} from 'antd';
 import TodoDialog from "./TodoDialog";
 import TodoList from "./TodoList";
+import {Tabs} from 'antd';
+
+const TabPane = Tabs.TabPane;
 
 class TodoBox extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            itemData: [
-                'hello world',
-                'second item'
+            allItem: [
+                'active one',
+                'completedItem one'
+            ],
+            activeItem: [
+                'active one'
+            ],
+            completedItem: [
+                'completedItem one'
             ]
         }
+    }
+
+    callback(key) {
+        console.log(key);
     }
 
     render() {
@@ -20,7 +33,17 @@ class TodoBox extends React.Component {
                 <Row>
                     <Col span={6} offset={9}>
                         <TodoDialog/>
-                        <TodoList itemData={this.state.itemData}/>
+                        <Tabs onChange={this.callback} type="card" tabPosition='bottom' tabBarGutter='20'>
+                            <TabPane tab="All" key="1">
+                                <TodoList itemData={this.state.allItem}/>
+                            </TabPane>
+                            <TabPane tab="Active" key="2">
+                                <TodoList itemData={this.state.activeItem}/>
+                            </TabPane>
+                            <TabPane tab="Completed" key="3">
+                                <TodoList itemData={this.state.completedItem}/>
+                            </TabPane>
+                        </Tabs>
                     </Col>
                 </Row>
             </div>
