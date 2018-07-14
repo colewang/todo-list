69709d272f6da5207b8e0b9c6f0ee39cc0502a0d
WANG Yu <464963515@qq.com>
2018-07-15 00:28:25 +0800

    [Initial Page Design][wangyu] Use respective for layout.

--- src/components/TodoBox.js
+++ src/components/TodoBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Row, Col} from 'antd';
 import TodoDialog from "./TodoDialog";
 import TodoList from "./TodoList";
-import {Tabs} from 'antd';
+import {Tabs, Divider} from 'antd';
 
 const TabPane = Tabs.TabPane;
 
@@ -29,24 +29,23 @@ class TodoBox extends React.Component {
 
     render() {
         return (
-            <div>
-                <Row>
-                    <Col span={6} offset={9}>
-                        <TodoDialog/>
-                        <Tabs onChange={this.callback} type="card" tabPosition='bottom' tabBarGutter='20'>
-                            <TabPane tab="All" key="1">
-                                <TodoList itemData={this.state.allItem}/>
-                            </TabPane>
-                            <TabPane tab="Active" key="2">
-                                <TodoList itemData={this.state.activeItem}/>
-                            </TabPane>
-                            <TabPane tab="Completed" key="3">
-                                <TodoList itemData={this.state.completedItem}/>
-                            </TabPane>
-                        </Tabs>
-                    </Col>
-                </Row>
-            </div>
+            <Row>
+                <Divider>Todo List</Divider>
+                <Col xs={{span: 18, offset: 3}} sm={{span: 14, offset: 5}} md={{span: 10, offset: 7}} lg={{span: 6, offset: 9}}>
+                    <TodoDialog/>
+                    <Tabs onChange={this.callback} type="card" tabPosition='bottom' tabBarGutter='20'>
+                        <TabPane tab="All" key="1">
+                            <TodoList itemData={this.state.allItem}/>
+                        </TabPane>
+                        <TabPane tab="Active" key="2">
+                            <TodoList itemData={this.state.activeItem}/>
+                        </TabPane>
+                        <TabPane tab="Completed" key="3">
+                            <TodoList itemData={this.state.completedItem}/>
+                        </TabPane>
+                    </Tabs>
+                </Col>
+            </Row>
         )
     }
 }
