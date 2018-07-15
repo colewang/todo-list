b0bdca390301a5efce57d01e8439575fe4b43a16
WANG Yu <464963515@qq.com>
2018-07-15 18:55:28 +0800

    [Do and Undo item][wangyu] Implemented do and undo item feature.

--- src/components/TodoBox.js
+++ src/components/TodoBox.js
@@ -12,6 +12,7 @@ class TodoBox extends React.Component {
         super(props);
         this.state = this.initData();
         this.handleAddItem = this.handleAddItem.bind(this);
+        this.handleTickItem = this.handleTickItem.bind(this);
     }
 
     initData() {
@@ -29,33 +30,65 @@ class TodoBox extends React.Component {
         this.state[target].push(value);
     }
 
+    removedDataById(target, id) {
+        this.state[target] = this.state[target].filter(itemInCompleted => {return itemInCompleted.id !== id})
+    }
+
+    findItemById(id) {
+        let returnValue = this.state.completedItem.filter(itemInCompleted => {return itemInCompleted.id === id});
+        return returnValue.length ? returnValue : this.state.activeItem.filter(itemInActive => {return itemInActive.id === id});
+    }
+
     handleAddItem(itemValue) {
         if (itemValue !== '') {
-            this.insertData('activeItem', {id: uuidv4(), value: itemValue, checked: false})
+            this.insertData('activeItem', {id: uuidv4(), value: itemValue, checked: false});
             this.setState(this.state);
         }
     }
 
     handleTickItem(event) {
-        console.log(event);
+        this.changeItemStatus(this.findItemById(event.target.id)[0]);
+        this.setState(this.state);
+    }
 
+    changeItemStatus(item) {
+        if(item.checked) {
+            this.handleTickCompletedItem(item);
+        } else {
+            this.handleTickActiveItem(item);
+        }
+        item.checked = !item.checked;
+    }
+
+    handleTickActiveItem(item) {
+        this.removedDataById('activeItem', item.id);
+        this.insertData('completedItem', item);
+    }
+
+    handleTickCompletedItem(item) {
+        this.removedDataById('completedItem', item.id);
+        this.insertData('activeItem', item);
     }
 
     render() {
         return (
             <Row>
                 <Divider>Todo List</Divider>
-                <Col xs={{span: 18, offset: 3}} sm={{span: 14, offset: 5}} md={{span: 10, offset: 7}} lg={{span: 6, offset: 9}}>
+                <Col xs={{span: 18, offset: 3}} sm={{span: 14, offset: 5}} md={{span: 10, offset: 7}}
+                     lg={{span: 6, offset: 9}}>
                     <TodoDialog handleAddItem={this.handleAddItem}/>
                     <Tabs type="card" tabPosition='bottom' tabBarGutter='20'>
                         <TabPane tab="All" key="1">
-                            <TodoList itemData={this.state.activeItem.concat(this.state.completedItem)} handleTickItem={this.handleTickItem}/>
+                            <TodoList itemData={this.state.activeItem.concat(this.state.completedItem)}
+                                      handleTickItem={this.handleTickItem}/>
                         </TabPane>
                         <TabPane tab="Active" key="2">
-                            <TodoList itemData={this.state.activeItem}/>
+                            <TodoList itemData={this.state.activeItem}
+                                      handleTickItem={this.handleTickItem}/>
                         </TabPane>
                         <TabPane tab="Completed" key="3">
-                            <TodoList itemData={this.state.completedItem}/>
+                            <TodoList itemData={this.state.completedItem}
+                                      handleTickItem={this.handleTickItem}/>
                         </TabPane>
                     </Tabs>
                 </Col>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ import {Checkbox} from 'antd';
 class TodoItem extends React.Component {
     render() {
         return (
-            <Checkbox id={this.props.item.id} onChange={this.props.handleTickItem} checked={this.props.item.checked}>{this.props.item.value}</Checkbox>
+            <Checkbox id={this.props.item.id} checked={this.props.item.checked} onChange={this.props.handleTickItem}>{this.props.item.value}</Checkbox>
         )
     }
 }
