009d8395e5d23175d24c1382d5bb3d212032ef9c
WANG Yu <464963515@qq.com>
2018-07-21 23:35:45 +0800

    [Add Unit Tests][wangyu] Added unit tests for TodoItem.

---
+++ test/components/TodoItem.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import TodoItem from "../../src/components/TodoItem";
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<TodoItem />', () => {
+    it('should render <TodoItem /> components', () => {
+        const wrapper = shallow(<TodoItem item={{id: 'uuid4test', value: 'testItem', checked: false, status: 'active'}}/>);
+        expect(wrapper.find('#uuid4test')).to.have.length(1);
+    });
+});
